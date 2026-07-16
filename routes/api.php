6e9a3d70eac0332c4376e72d4003f3d33ef10fd0
Francis <brakay1136@gmail.com>
2026-07-16 09:34:42 -0800

    Token based Authentication for user register, login and logout created

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AuthenticationController;
 use App\Http\Controllers\Api\ExpensesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('expenses', ExpensesController::class);
+Route::post('register', [AuthenticationController::class, 'register']);
+Route::post('login', [AuthenticationController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::apiResource('expenses', ExpensesController::class);
+
+});
+
